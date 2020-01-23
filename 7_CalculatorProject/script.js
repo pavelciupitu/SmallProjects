81a9501e1f6abf409b1d10bd718efa3d1c517b3c
Pavel Ciupitu <pavel.ciupitu@gmail.com>
2020-01-23 14:00:36 +0200

    Adding the new calculator !!

---
+++ 7_CalculatorProject/script.js
@@ -0,0 +1,26 @@
+// select elements
+
+const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector(".btn-equal");
+const clearBtn = document.querySelector(".btn-clear");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(){
+        let number = btn.getAttribute("data-num");
+        screen.value +=number;
+    });
+});
+
+equalBtn.addEventListener ("click", function(){
+    if(screen.value === "") {
+        screen.value = "Nothing to calculate"
+    } else {
+        let value= eval(screen.value);
+        screen.value = value;
+    }
+});
+
+clearBtn.addEventListener("click", function() {
+    screen.value= "";
+});
